03fc7853c226fb5d64c7a76324316b3913b7933e
arjungautam1 <arjungautam5431@gmail.com>
2022-06-05 19:36:55 +0545

    user service implementation completed

--- src/main/java/com/codewitharjun/fullstackecommerce/service/UserService.java
+++ src/main/java/com/codewitharjun/fullstackecommerce/service/UserService.java
@@ -1,7 +1,17 @@
 package com.codewitharjun.fullstackecommerce.service;
 
+import com.codewitharjun.fullstackecommerce.model.Role;
 import com.codewitharjun.fullstackecommerce.model.User;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
 
 public interface UserService {
     User saveUser(User user);
+
+    Optional<User> findUserByUserName(String username);
+
+    @Transactional
+        //Transaction is required when executing an update/delete query.
+    void changeRole(String username, Role newRole);
 }
--- src/main/java/com/codewitharjun/fullstackecommerce/service/UserServiceImpl.java
+++ src/main/java/com/codewitharjun/fullstackecommerce/service/UserServiceImpl.java
@@ -6,7 +6,9 @@ import com.codewitharjun.fullstackecommerce.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -28,5 +30,18 @@ public class UserServiceImpl implements UserService {
 
         return userRepository.save(user);
     }
+
+    @Override
+    public Optional<User> findUserByUserName(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    @Transactional //Transaction is required when executing an update/delete query.
+    public void changeRole(String username,Role newRole) {
+        userRepository.updateUserRole(username, newRole);
+    }
+
+
 
 }
